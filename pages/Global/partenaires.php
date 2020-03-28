fd857adfee6b3a509d1490bdf18bbaf9493d5f54
Dis-web <santalucia.cedric@gmail.com>
2020-03-28 21:21:59 +0100

    premiere sauvegarde

---
+++ pages/Global/partenaires.php
@@ -0,0 +1,19 @@
+<?php include("../Commons/header.php") ?>
+
+<h2 class='text-center mt-3 perso_ColorRoseMenu perso_policeTitre perso_textShadow'>Les partenaires</h2>
+<div class="row no-gutters">
+    <div class="card col-auto mx-auto mt-2" style="width: 18rem;">
+        <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="UPDP">
+        <div class="card-body text-center">
+            <h5 class="card-title perso_ColorRoseMenu perso_policeTitre perso_textShadow">UPDP 09</h5>
+            <p class="card-text">
+                Contactez un éducateur canin, contactez UPDP 09
+            </p>
+            <a href="http://www.updp-09.fr/" target=_blank class="btn btn-primary">Visiter le site de l'éducatrice &raquo;</a>
+        </div>
+    </div>
+</div>
+
+<?php include("../Commons/footer.php") ?>
+            
+      
