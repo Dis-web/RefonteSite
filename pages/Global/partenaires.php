b374da91ae860754bad5ee9e260974ad135b4338
Dis-web <santalucia.cedric@gmail.com>
2020-03-30 23:12:30 +0200

    nouveau dossier fichier function php

--- pages/Global/partenaires.php
+++ pages/Global/partenaires.php
@@ -1,6 +1,8 @@
-<?php include("../Commons/header.php") ?>
+<?php
+include("../../utile/formatage.php");
+include("../Commons/header.php"); ?>
 
-<h2 class='text-center mt-3 perso_ColorRoseMenu perso_policeTitre perso_textShadow'>Les partenaires</h2>
+<?= styleTitreNiveau1("Les partenaires", "perso_ColorRoseMenu") ?>
 <div class="row no-gutters">
     <div class="card col-auto mx-auto mt-2" style="width: 18rem;">
         <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="UPDP">
@@ -9,11 +11,12 @@
             <p class="card-text">
                 Contactez un éducateur canin, contactez UPDP 09
             </p>
-            <a href="http://www.updp-09.fr/" target=_blank class="btn btn-primary">Visiter le site de l'éducatrice &raquo;</a>
+            <a href="http://www.updp-09.fr/" target=_blank class="btn btn-primary">Visiter le site de l'éducatrice
+                &raquo;</a>
         </div>
     </div>
 </div>
 
-<?php include("../Commons/footer.php") ?>
-            
-      
+<?php
+include("../Commons/footer.php");
+?>
